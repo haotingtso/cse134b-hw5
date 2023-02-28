05e67661304183d8c1a8ad80fc37e2c0bb15e5c5
davidtso1219 <davidtso1219@gmail.com>
2023-02-28 13:51:14 -0800

    Modify structure in part two

--- customdialog.js
+++ customdialog.js
@@ -1,110 +1,206 @@
-let alert_btn = document.getElementById("alert-btn");
-let confirm_btn = document.getElementById("confirm-btn");
-let prompt_btn = document.getElementById("prompt-btn");
-let output = document.querySelector("output");
-let dialog = document.querySelector("dialog");
+export const customAlert = message => {
+  // create dialog fragment
+  let template = document.getElementById("alert-template");
+  let dialog = template.content.cloneNode(true);
 
-function main() {
-  addEventListeners();
-  modifyEventListeners();
-}
+  // select fragment child
+  let button = dialog.querySelector("button");
+  let alert_message = dialog.querySelector("#alert-message");
 
-function modifyEventListeners() {
-  window.alert = message => {
-    let template = document.getElementById("alert-template");
-    let clone = template.content.cloneNode(true);
-    let button = clone.querySelector("button");
+  // append dialog element to the body
+  document.body.appendChild(dialog);
 
-    button.addEventListener("click", closeDialog);
-
-    dialog.appendChild(document.createTextNode(message));
-    dialog.appendChild(clone);
-    dialog.showModal();
-  };
-
-  window.confirm = (message, onclick = closeDialog) => {
-    let template = document.getElementById("confirm-template");
-    let clone = template.content.cloneNode(true);
-
-    let buttons = clone.querySelectorAll("button");
-    let [cancel, ok] = buttons;
-
-    cancel.addEventListener("click", () => onclick(false));
-    ok.addEventListener("click", () => onclick(true));
-
-    dialog.appendChild(document.createTextNode(message));
-    dialog.appendChild(clone);
-    dialog.showModal();
-  };
-
-  window.prompt = (message, onclick = closeDialog) => {
-    let template = document.getElementById("prompt-template");
-    let clone = template.content.cloneNode(true);
-
-    let control = clone.querySelector(".control");
-    let input = clone.querySelector("input");
-    let buttons = clone.querySelectorAll("button");
-    let [cancel, ok] = buttons;
-
-    cancel.addEventListener("click", () => onclick(""));
-    ok.addEventListener("click", () => onclick(input.value));
-
-    control.insertBefore(document.createTextNode(message), control.firstChild);
-    dialog.appendChild(clone);
-    dialog.showModal();
-  };
-}
-
-function addEventListeners() {
-  alert_btn.addEventListener("click", () => {
-    setOutput("");
-    setTimeout(() => {
-      window.alert("Alert pressed!");
-    }, 0);
+  // get the dialog element and add a message and an event listener to the button
+  dialog = document.querySelector("dialog");
+  alert_message.appendChild(document.createTextNode(message));
+  button.addEventListener("click", () => {
+    document.body.removeChild(dialog);
   });
+  dialog.showModal();
+};
 
-  confirm_btn.addEventListener("click", () => {
-    setOutput("");
+export const customConfirm = (message, onclick = closeDialog) => {
+  // create dialog fragment
+  let template = document.getElementById("confirm-template");
+  let dialog = template.content.cloneNode(true);
 
-    const onclick = isOk => {
-      setOutput(`Confirm result : ${isOk}`);
-      closeDialog();
-    };
+  // select fragment child
+  let [cancel, ok] = dialog.querySelectorAll("button");
+  let confirm_message = dialog.querySelector("#confirm-message");
 
-    setTimeout(() => {
-      window.confirm("Can you confirm this?", onclick);
-    }, 0);
+  // append dialog element to the body
+  document.body.appendChild(dialog);
+
+  // get the dialog element and add a message and event listeners to the buttons
+  dialog = document.querySelector("dialog");
+  confirm_message.appendChild(document.createTextNode(message));
+  cancel.addEventListener("click", () => {
+    onclick(false);
+    document.body.removeChild(dialog);
   });
-
-  prompt_btn.addEventListener("click", () => {
-    setOutput("");
-
-    const onclick = message => {
-      message = cleanUserInput(message);
-      setOutput(
-        message ? `Prompt result : ${message}` : "User didn't enter anything"
-      );
-      closeDialog();
-    };
-
-    setTimeout(() => {
-      window.prompt("What is your name?", onclick);
-    }, 0);
+  ok.addEventListener("click", () => {
+    onclick(true);
+    document.body.removeChild(dialog);
   });
-}
+  dialog.showModal();
+};
 
-function cleanUserInput(userInput) {
-  return DOMPurify.sanitize(userInput);
-}
+export const customPrompt = (message, onclick = closeDialog) => {
+  // create dialog fragment
+  let template = document.getElementById("prompt-template");
+  let dialog = template.content.cloneNode(true);
 
-function setOutput(newContent) {
-  output.innerHTML = newContent;
-  output.style.border = newContent ? "10px double black" : "";
-}
+  // select fragment child
+  let label = dialog.querySelector("label");
+  let input = dialog.querySelector("input");
+  let [cancel, ok] = dialog.querySelectorAll("button");
 
-function closeDialog() {
-  dialog.innerHTML = ``;
-  dialog.close();
-}
+  // append dialog element to the body
+  document.body.appendChild(dialog);
 
-main();
+  // get the dialog element and add a message and event listeners to the buttons
+  dialog = document.querySelector("dialog");
+  label.appendChild(document.createTextNode(message));
+  cancel.addEventListener("click", () => {
+    onclick("");
+    document.body.removeChild(dialog);
+  });
+  ok.addEventListener("click", () => {
+    onclick(input.value);
+    document.body.removeChild(dialog);
+  });
+  dialog.showModal();
+};
+
+// let alert_btn = document.getElementById("alert-btn");
+// let confirm_btn = document.getElementById("confirm-btn");
+// let prompt_btn = document.getElementById("prompt-btn");
+// let output = document.querySelector("output");
+
+// function main() {
+//   addEventListeners();
+//   modifyEventListeners();
+// }
+
+// function modifyEventListeners() {
+//   window.alert = message => {
+//     // create dialog fragment
+//     let template = document.getElementById("alert-template");
+//     let dialog = template.content.cloneNode(true);
+
+//     // select fragment child
+//     let button = dialog.querySelector("button");
+//     let alert_message = dialog.querySelector("#alert-message");
+
+//     // append dialog element to the body
+//     document.body.appendChild(dialog);
+
+//     // get the dialog element and add a message and an event listener to the button
+//     dialog = document.querySelector("dialog");
+//     alert_message.appendChild(document.createTextNode(message));
+//     button.addEventListener("click", () => {
+//       document.body.removeChild(dialog);
+//     });
+//     dialog.showModal();
+//   };
+
+//   window.confirm = (message, onclick = closeDialog) => {
+//     // create dialog fragment
+//     let template = document.getElementById("confirm-template");
+//     let dialog = template.content.cloneNode(true);
+
+//     // select fragment child
+//     let [cancel, ok] = dialog.querySelectorAll("button");
+//     let confirm_message = dialog.querySelector("#confirm-message");
+
+//     // append dialog element to the body
+//     document.body.appendChild(dialog);
+
+//     // get the dialog element and add a message and event listeners to the buttons
+//     dialog = document.querySelector("dialog");
+//     confirm_message.appendChild(document.createTextNode(message));
+//     cancel.addEventListener("click", () => {
+//       onclick(false);
+//       document.body.removeChild(dialog);
+//     });
+//     ok.addEventListener("click", () => {
+//       onclick(true);
+//       document.body.removeChild(dialog);
+//     });
+//     dialog.showModal();
+//   };
+
+//   window.prompt = (message, onclick = closeDialog) => {
+//     // create dialog fragment
+//     let template = document.getElementById("prompt-template");
+//     let dialog = template.content.cloneNode(true);
+
+//     // select fragment child
+//     let label = dialog.querySelector("label");
+//     let input = dialog.querySelector("input");
+//     let [cancel, ok] = dialog.querySelectorAll("button");
+
+//     // append dialog element to the body
+//     document.body.appendChild(dialog);
+
+//     // get the dialog element and add a message and event listeners to the buttons
+//     dialog = document.querySelector("dialog");
+//     label.appendChild(document.createTextNode(message));
+//     cancel.addEventListener("click", () => {
+//       onclick("");
+//       document.body.removeChild(dialog);
+//     });
+//     ok.addEventListener("click", () => {
+//       onclick(input.value);
+//       document.body.removeChild(dialog);
+//     });
+//     dialog.showModal();
+//   };
+// }
+
+// function addEventListeners() {
+//   alert_btn.addEventListener("click", () => {
+//     setOutput("");
+//     setTimeout(() => {
+//       window.alert("Alert pressed!");
+//     }, 0);
+//   });
+
+//   confirm_btn.addEventListener("click", () => {
+//     setOutput("");
+
+//     const onclick = isOk => {
+//       setOutput(`Confirm result : ${isOk}`);
+//     };
+
+//     setTimeout(() => {
+//       window.confirm("Can you confirm this?", onclick);
+//     }, 0);
+//   });
+
+//   prompt_btn.addEventListener("click", () => {
+//     setOutput("");
+
+//     const onclick = message => {
+//       message = cleanUserInput(message);
+//       setOutput(
+//         message ? `Prompt result : ${message}` : "User didn't enter anything"
+//       );
+//     };
+
+//     setTimeout(() => {
+//       window.prompt("What is your name?", onclick);
+//     }, 0);
+//   });
+// }
+
+// function cleanUserInput(userInput) {
+//   return DOMPurify.sanitize(userInput);
+// }
+
+// function setOutput(newContent) {
+//   output.innerHTML = newContent;
+//   output.style.border = newContent ? "10px double black" : "";
+// }
+
+// main();
