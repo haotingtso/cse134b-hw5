a3abc44751cd8e1a5ddd654e59faff208319941a
davidtso1219 <davidtso1219@gmail.com>
2023-03-01 14:31:52 -0800

    Added LS features

--- blog.js
+++ blog.js
@@ -1,4 +1,5 @@
-let posts = [];
+const LOCAL_STORAGE_KEY = "blog-posts";
+let posts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 
 export const addPost = ({ title, date, summary }) => {
   posts.push({
@@ -6,14 +7,21 @@ export const addPost = ({ title, date, summary }) => {
     date,
     summary,
   });
+  updateLocalStorage();
 };
 
 export const editPost = (index, new_post) => {
   posts[index] = new_post;
+  updateLocalStorage();
 };
 
 export const deletePost = index => {
   posts.splice(index, 1);
+  updateLocalStorage();
+};
+
+const updateLocalStorage = () => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(posts));
 };
 
 export default posts;
