59134d16b0146051b7e91b52b59a39b2b6e7046c
davidtso1219 <davidtso1219@gmail.com>
2023-03-02 19:01:19 -0800

    Refactoring custom dialogs to leverage dialog and form

--- customdialog.js
+++ customdialog.js
@@ -1,112 +1,182 @@
 export const customAlert = message => {
-  // create dialog fragment
-  let template = document.getElementById("alert-template");
-  let dialog = template.content.cloneNode(true);
-
-  // select fragment child
-  let button = dialog.querySelector("button");
-  let alert_message = dialog.querySelector("#alert-message");
-
-  // modify content and add event listener
-  alert_message.appendChild(document.createTextNode(message));
-  button.addEventListener("click", () => {
-    closeDialog();
+  openDialog(alertMarkup(message));
+  setTimeout(() => {
+    let button = document.querySelector("dialog button");
+    button.addEventListener("click", () => {
+      closeDialog();
+    });
   });
-
-  openDialog(dialog);
 };
+
+const alertMarkup = message => `
+  <dialog>
+    <p>${message}</p>
+    <button>Ok</button>
+  </dialog>
+`;
 
 export const customConfirm = (message, onclick) => {
-  // create dialog fragment
-  let template = document.getElementById("confirm-template");
-  let dialog = template.content.cloneNode(true);
-
-  // select fragment child
-  let [cancel, ok] = dialog.querySelectorAll("button");
-  let confirm_message = dialog.querySelector("#confirm-message");
-
-  // modify content and add event listener
-  confirm_message.appendChild(document.createTextNode(message));
-  cancel.addEventListener("click", () => {
-    onclick(false);
-    closeDialog();
-  });
-  ok.addEventListener("click", () => {
-    onclick(true);
-    closeDialog();
-  });
-
-  openDialog(dialog);
-};
-
-export const customPrompt = (message, onclick) => {
-  // create dialog fragment
-  let template = document.getElementById("prompt-template");
-  let dialog = template.content.cloneNode(true);
-
-  // select fragment child
-  let label = dialog.querySelector("label");
-  let input = dialog.querySelector("input");
-  let [cancel, ok] = dialog.querySelectorAll("button");
-
-  // modify content and add event listener
-  label.appendChild(document.createTextNode(message));
-  cancel.addEventListener("click", () => {
-    onclick("");
-    closeDialog();
-  });
-  ok.addEventListener("click", () => {
-    onclick(input.value);
-    closeDialog();
-  });
-
-  openDialog(dialog);
-};
-
-export function showPostPrompt({
-  post = { title: "", date: "", summary: "" },
-  onSave = closeDialog,
-}) {
-  // create dialog fragment
-  let template = document.getElementById("post-prompt-template");
-  let dialog = template.content.cloneNode(true);
-
-  // select fragment child
-  let title_input = dialog.querySelector("#post-title");
-  let date_input = dialog.querySelector("#post-date");
-  let summary_input = dialog.querySelector("#post-summary");
-  let [cancel, save] = dialog.querySelectorAll("button");
-
-  // modify content and add event listener
-  let { title, date, summary } = post;
-  title_input.value = title;
-  date_input.value = date;
-  summary_input.value = summary;
-
-  cancel.addEventListener("click", () => {
-    closeDialog();
-  });
-
-  save.addEventListener("click", () => {
-    onSave({
-      title: title_input.value,
-      date: date_input.value,
-      summary: summary_input.value,
-    });
-    closeDialog();
-  });
-
-  openDialog(dialog);
-}
-
-function openDialog(dialog) {
-  let existing_dialog = document.querySelector("dialog[open]");
-  if (existing_dialog) {
-    document.body.removeChild(existing_dialog);
-  }
+  openDialog(confirmMarkup(message));
 
   setTimeout(() => {
-    document.body.appendChild(dialog);
+    let dialog = document.querySelector("dialog");
+
+    dialog.addEventListener("close", () => {
+      onclick(dialog.returnValue === "true");
+      closeDialog();
+    });
+  });
+};
+
+const confirmMarkup = message => `
+<dialog>
+  <form method="dialog">
+    <p>${message}</p>
+    <div class="buttons">
+      <button value="false">Cancel</button>
+      <button value="true">Ok</button>
+    </div>
+  </form>
+</dialog>
+`;
+
+export const customPrompt = (message, onclick) => {
+  openDialog(promptMarkup(message));
+
+  setTimeout(() => {
+    let dialog = document.querySelector("dialog");
+    let prompt = dialog.querySelector("input");
+
+    dialog.addEventListener("close", () => {
+      onclick(dialog.returnValue === "true" && prompt.value);
+      closeDialog();
+    });
+  });
+};
+
+const promptMarkup = message => `
+<dialog>
+  <form method="dialog">
+    <div class="field">
+      <label for="prompt">${message}</label>
+      <input type="text" id="prompt" name="prompt" />
+    </div>
+    <div class="buttons">
+      <button value="false">Cancel</button>
+      <button value="true">Ok</button>
+    </div>
+  </form>
+</dialog>
+`;
+
+export function showPostPrompt({ post, onSave }) {
+  openDialog(postPromptMarkup(post));
+  setTimeout(() => addPostPromptListener(onSave));
+}
+
+const postPromptMarkup = (post = { title: "", date: "", summary: "" }) => `
+<dialog>
+    <form method="dialog">
+      <div class="form-field">
+        <label for="post-title">Title:</label>
+        <input
+          type="text"
+          id="post-title"
+          name="post-title"
+          value="${post.title}"
+          required
+        />
+      </div>
+      <div class="form-field">
+        <label for="post-date">Date:</label>
+        <input
+          type="date"
+          id="post-date"
+          name="post-date"
+          value="${post.date}"
+          required
+        />
+      </div>
+      <div class="form-field">
+        <label for="post-summary"></label>
+        <input
+          type="text"
+          id="post-summary"
+          name="post-summary"
+          value="${post.summary}"
+          required
+        />
+      </div>
+      <div class="form-field">
+        <button value="false">Cancel</button>
+        <button value="true">Save</button>
+      </div>
+    </form>
+  </dialog>
+`;
+
+export function showStyledPostPrompt({ post, onSave }) {
+  openDialog(styledPostPromptMarkup(post));
+  setTimeout(() => addPostPromptListener(onSave));
+}
+
+const styledPostPromptMarkup = (
+  post = { title: "", date: "", summary: "" }
+) => `
+<dialog>
+  <form method="dialog">
+    <input
+      type="text"
+      id="post-title"
+      name="post-title"
+      value="${post.title}"
+      placeholder="Post Title"
+      required
+    />
+    <input
+      type="date"
+      id="post-date"
+      name="post-date"
+      value="${post.date}"
+      required
+    />
+    <textarea
+      type="text"
+      id="post-summary"
+      name="post-summary"
+      placeholder="Post Summary"
+      required
+    >${post.summary}</textarea>
+    <div class="buttons">
+      <button value="false">Cancel</button>
+      <button value="true">Save</button>
+    </div>
+  </form>
+</dialog>
+`;
+
+const addPostPromptListener = onSave => {
+  let dialog = document.querySelector("dialog");
+  let title = dialog.querySelector("#post-title");
+  let date = dialog.querySelector("#post-date");
+  let summary = dialog.querySelector("#post-summary");
+
+  dialog.addEventListener("close", () => {
+    if (dialog.returnValue === "true") {
+      onSave({
+        title: title.value,
+        date: date.value,
+        summary: summary.value,
+      });
+    }
+    closeDialog();
+  });
+};
+
+function openDialog(dialog) {
+  closeDialog();
+  setTimeout(() => {
+    document.body.insertAdjacentHTML("beforeend", dialog);
     document.querySelector("dialog").showModal();
   }, 0);
 }
